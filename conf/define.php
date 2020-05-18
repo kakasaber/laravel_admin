32ba10a4fc7f4f4f1e57a6283432f488d3dc6b74
kai <839823628@qq.com>
2020-05-18 15:42:08 +0800

    second commit

---
+++ conf/define.php
@@ -0,0 +1,10 @@
+<?php
+
+define("DS", '/');
+define("ROOT_PATH",  realPath(__DIR__.DS.'..'.DS).DS);
+define("APPLICATION_PATH",  realPath(__DIR__.DS.'..'.DS.'application').DS);
+define("TEMP_PATH",  realpath(__DIR__.DS.'..'.DS.'htdocs'.DS.'temp'));
+define("SEEDIT_DB_MYSQL_001_HOST","127.0.0.1");
+define("SEEDIT_DB_MYSQL_001_PORT","3306");
+define("SEEDIT_DB_MYSQL_001_USERNAME","root");
+define("SEEDIT_DB_MYSQL_001_PASSWORD","123456");
